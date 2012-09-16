b08ab75e7cef4f4431592a6cddcc34f502284ef0
navedalam <alam.naved18@gmail.com>
2012-09-17 05:02:44 +0530

    Update models.js

--- models.js
+++ models.js
@@ -3,24 +3,23 @@ mongoose.connect('mongodb://localhost/test');
 var Schema = mongoose.Schema;
 
 var Pending = new Schema({
-    fileName    : String
-  , Uploader    : String
-  , hash        : String
-  , transferID  : Number
+    fileName  : String
+  , Uploader  : String
+  , hash      : String
 });
 
 var UserSchema = new Schema({
-    mac        	    : ObjectId
-  , ip     	    : String
-  , nick      	    : String
-  , spaceAllocated  : Number
-  , state 	    : Boolean
-  , pending 	    : [Pending]
-  , dataUploaded : Number
-  , dataDownloaded: Number
+    mac        		: ObjectId
+  , ip     			: String
+  , nick      		: String
+  , spaceAllocated 	: Number
+  , state 			: Boolean
+  , pending  		: [Pending]
+  , dataUploaded 	: Number
+  , dataDownloaded 	: Number
 });
 
-var FileSchema = new Schema({
+var Files = new Schema({
     hash	: ObjectId
   , name    : String
   , size    : String
