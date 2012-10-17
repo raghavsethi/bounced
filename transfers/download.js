938fbb73ec7c62593dff20e5e74b7f8b702074c3
Naved Alam <alam.naved18@gmail.com>
2012-10-18 03:27:36 +0530

    Completed Download
    @Raghav - My sync was never broken

--- transfers/download.js
+++ transfers/download.js
@@ -1,6 +1,7 @@
-﻿var encryption = require('./encryption');
+var encryption = require('./encryption');
 
 Pending = require('../models').Pending;
+Friendship = require('../models').Friendship;
 
 function downloadHandler(req, res) {
 
@@ -26,56 +27,51 @@ function downloadHandler(req, res) {
         newPending.fileSize = requestFileSize;
         newPending.uploader = requestMac;
         newPending.downloader = requestDownloader;
-        newPending.transferID = encryption.generateNewId();
+        //newPending.transferID = encryption.generateNewId();
         newPending.symKey = encryption.generateNewKey();
         newPending.uploaderIP = "invalid";
-
-        // Now behaviour will diverge depending on whether this is an online
+		newPending.nick = 'He Who Must Not Named';
+		newPending.type = "direct";
+        //newPending.save();
+		// Now behaviour will diverge depending on whether this is an online
         // or offline transfer
 
         // Online
-        if (requestType.toLowerCase() === "direct") {
-            newPending.online = true;
-            newPending.type = "direct";
-        }
-        // Offline
-        else {
-            newPending.online = false;
-            newPending.type = "firstleg";
-			var newfriendPendings[];
-			var queryParams = {
-				$or: [{ 'friend1': currentOnlineUser.mac, 'friend2': currentUser.mac },
-				{ 'friend1': currentUser.mac, 'friend2': currentOnlineUser.mac }]
-			};
-			Friendship.find(queryParams, function (error, friends).sort(count:1).limit(5) {
-				if(friends.length==0){
-					res.send({'status':'Error', 'text':'error'});
+		var bounced=[];
+        if (requestType.toLowerCase() == "bounced") {
+            console.log("HERE");
+			Friendship.find({$or: [{ 'friend1': requestDownloader },{ 'friend2': requestDownloader }]},{},{limit:1, sort:[['count','desc']]}, function(error, friends){
+				console.log("Determined Friends");
+				for(var i=0;i<friends.length;i++){
+				
+					var bouncedPending = new Pending();
+					bouncedPending.fileHash = requestFileHash;
+					bouncedPending.fileName = requestFileName;
+					bouncedPending.fileSize = requestFileSize;
+					bouncedPending.uploader = requestMac;
+					if(friends[0].friend1 == requestMac)
+						bouncedPending.downloader = friends[0].friend2;
+					else
+						bouncedPending.downloader = friends[0].friend1;
+					bouncedPending.uploaderIP = "invalid";
+					bouncedPending.nick = 'He Who Must Not Named';
+					bouncedPending.type = "firstleg";
+					console.log(bouncedPending)
+					bounced.push(bouncedPending);
+				
 				}
-				for (friend in friends){
-					var newPending = new Pending();
-
-					newPending.fileHash = requestFileHash;
-					newPending.fileName = requestFileName;
-					newPending.fileSize = requestFileSize;
-					newPending.uploader = requestMac;
-					newPending.downloader = friend.;
-					newPending.transferID = encryption.generateNewId();
-					newPending.symKey = encryption.generateNewKey();
-					newPending.uploaderIP = "invalid";
-							
-				}
-			
+        
 			});
-
-            // Add 'pending's for x friends
-
-            //TODO: Complete this method.
-        }
+			
+		}
+		        
 
         Pending.find({ 'fileHash': newPending.fileHash, 'uploader': newPending.uploader, 'type': newPending.type }, function (error, users) {
             if (error == null) {
                 if (users.length == 0) {
                     newPending.save();
+					for(var i=0; i<bounced.length;i++)
+						bounced[i].save();
                     res.send({ 'status': 'OK', 'text': 'Download request accepted' })
                 }
                 else {
