4d668953842f9cef2cf3fc13337c8213edbd56de
Raghav Sethi <raghavsethi.rs@gmail.com>
2012-10-20 15:52:53 +0530

    Fixes to download and update

--- transfers/download.js
+++ transfers/download.js
@@ -27,51 +27,54 @@ function downloadHandler(req, res) {
         newPending.fileSize = requestFileSize;
         newPending.uploader = requestMac;
         newPending.downloader = requestDownloader;
-        //newPending.transferID = encryption.generateNewId();
+        newPending.transferID = encryption.generateNewId();
         newPending.symKey = encryption.generateNewKey();
         newPending.uploaderIP = "invalid";
-		newPending.nick = 'He Who Must Not Named';
-		newPending.type = "direct";
+        newPending.nick = 'He Who Must Not Named';
+        newPending.type = "direct";
+        console.log("\n\n");
+        console.log(newPending);
         //newPending.save();
-		// Now behaviour will diverge depending on whether this is an online
+        // Now behaviour will diverge depending on whether this is an online
         // or offline transfer
 
         // Online
-		var bounced=[];
+        var bounced = [];
         if (requestType.toLowerCase() == "bounced") {
             console.log("HERE");
-			Friendship.find({$or: [{ 'friend1': requestDownloader },{ 'friend2': requestDownloader }]},{},{limit:1, sort:[['count','desc']]}, function(error, friends){
-				console.log("Determined Friends");
-				for(var i=0;i<friends.length;i++){
-				
-					var bouncedPending = new Pending();
-					bouncedPending.fileHash = requestFileHash;
-					bouncedPending.fileName = requestFileName;
-					bouncedPending.fileSize = requestFileSize;
-					bouncedPending.uploader = requestMac;
-					if(friends[0].friend1 == requestMac)
-						bouncedPending.downloader = friends[0].friend2;
-					else
-						bouncedPending.downloader = friends[0].friend1;
-					bouncedPending.uploaderIP = "invalid";
-					bouncedPending.nick = 'He Who Must Not Named';
-					bouncedPending.type = "firstleg";
-					console.log(bouncedPending)
-					bounced.push(bouncedPending);
-				
-				}
-        
-			});
-			
-		}
-		        
+            Friendship.find({ $or: [{ 'friend1': requestDownloader }, { 'friend2': requestDownloader}] }, {}, { limit: 1, sort: [['count', 'desc']] }, function (error, friends) {
+                console.log("Determined Friends");
+                for (var i = 0; i < friends.length; i++) {
+
+                    var bouncedPending = new Pending();
+                    bouncedPending.fileHash = requestFileHash;
+                    bouncedPending.fileName = requestFileName;
+                    bouncedPending.transferID = newPending.transferID;
+                    bouncedPending.fileSize = requestFileSize;
+                    bouncedPending.uploader = requestMac;
+                    if (friends[0].friend1 == requestMac)
+                        bouncedPending.downloader = friends[0].friend2;
+                    else
+                        bouncedPending.downloader = friends[0].friend1;
+                    bouncedPending.uploaderIP = "invalid";
+                    bouncedPending.nick = 'He Who Must Not Named';
+                    bouncedPending.type = "firstleg";
+                    console.log(bouncedPending)
+                    bounced.push(bouncedPending);
+
+                }
+
+            });
+
+        }
+
 
         Pending.find({ 'fileHash': newPending.fileHash, 'uploader': newPending.uploader, 'type': newPending.type }, function (error, users) {
             if (error == null) {
                 if (users.length == 0) {
                     newPending.save();
-					for(var i=0; i<bounced.length;i++)
-						bounced[i].save();
+                    for (var i = 0; i < bounced.length; i++)
+                        bounced[i].save();
                     res.send({ 'status': 'OK', 'text': 'Download request accepted' })
                 }
                 else {
--- transfers/update.js
+++ transfers/update.js
@@ -7,91 +7,92 @@ function updateHandler(req, res){
 	var newHash=req.body.newHash;
 
 	User.find({ 'ip': req.ip }, function (error, users) {
-		console.log("tid "+tID+" status "+status+" newHash "+newHash +" result "+users);
-		if (users == undefined || users.length == 0) {
-            console.log('Cannot find user with IP ' + req.ip);
-            res.send({ 'status': 'Error', 'text': 'Cannot find user with IP ' + req.ip });
-            return;
-        }
-		var mac=users[0].mac
-		console.log(mac);
-		Pending.find({'transferID':tID, 'downloader':mac},{'type':1},function(error, updatingClient){
-			console.log(updatingClient);
-			var type=updatingClient[0].type;
-			console.log(type);
-			
-			if(type == 'direct' || type == 'secondleg'){
-			
-				Pending.find({'transferID': tID}, {'uploader':1, 'fileHash':1, 'type':1},function(error, requests){
-				
-					Pending.remove({"transferID": tID},function(err, removed){
-						console.log(removed);
-					});
-					
-					for(var i=0;i<requests.length;i++){
-					
-						if(requests[i].type=='secondleg')
-						{
-							var newPending = new Pending();
-							newPending.fileHash = requests[i].fileHash;
-							newPending.downloader=requests[i].uploader;
-							newPending.transferID=tID;
-							newPending.type='delete';
-							
-							newPending.save();
-							console.log("Pending to delete file added");
-							console.log(newPending);
-						}
-					}	
-					res.send({'status': 'OK', 'text':'Update Complete'});
-					
-					
-				
-				});
-			}
-			
-			if(type == 'firstleg'){
-				
-				
-				if(status == 'done'){
-					
-					Pending.find({'transferID': tID, 'type':'direct'}, function(error, request){
-						console.log("here");
-								
-						var newPending = new Pending();
-						newPending.fileHash = newHash;
-						newPending.downloader=request[0].downloader;
-						newPending.uploader=mac;
-						newPending.fileName=request[0].fileName;
-						newPending.fileName=request[0].fileName;
-						newPending.transferID=tID;
-						newPending.type='secondleg';
-						
-						newPending.save();
-						console.log("Pending to delete file added");
-						console.log(newPending);
-					});		
-				}
-				
-				Pending.remove({"transferID": tID, 'downloader':mac, 'type':'firstleg'},function(err, removed){
-					console.log(removed);
-					res.send({'status': 'OK', 'text':'Update Complete'});
-				});
-					
-			}
-			
-			if(type == 'delete'){
-			
-				Pending.remove({"transferID": tID, 'downloader':mac, 'type':'delete'},function(err, removed){
-					console.log(removed);
-					res.send({'status': 'OK', 'text':'Update Complete'});
-				});
-					
-			}
-			
-			
-		});
-		
+	    console.log("tid " + tID + " status " + status + " newHash " + newHash + " result " + users);
+	    if (users == undefined || users.length == 0) {
+	        console.log('Cannot find user with IP ' + req.ip);
+	        res.send({ 'status': 'Error', 'text': 'Cannot find user with IP ' + req.ip });
+	        return;
+	    }
+	    var mac = users[0].mac
+	    console.log(mac);
+	    console.log("downloader" + mac);
+	    console.log("tID" + tID);
+	    Pending.find({ 'transferID': tID, 'downloader': mac }, { 'type': 1 }, function (error, updatingClient) {
+	        console.log(updatingClient);
+	        var type = updatingClient[0].type;
+	        console.log(type);
+
+	        if (type == 'direct' || type == 'secondleg') {
+
+	            Pending.find({ 'transferID': tID }, { 'uploader': 1, 'fileHash': 1, 'type': 1 }, function (error, requests) {
+
+	                Pending.remove({ "transferID": tID }, function (err, removed) {
+	                    console.log(removed);
+	                });
+
+	                for (var i = 0; i < requests.length; i++) {
+
+	                    if (requests[i].type == 'secondleg') {
+	                        var newPending = new Pending();
+	                        newPending.fileHash = requests[i].fileHash;
+	                        newPending.downloader = requests[i].uploader;
+	                        newPending.transferID = tID;
+	                        newPending.type = 'delete';
+
+	                        newPending.save();
+	                        console.log("Pending to delete file added");
+	                        console.log(newPending);
+	                    }
+	                }
+	                res.send({ 'status': 'OK', 'text': 'Update Complete' });
+
+
+
+	            });
+	        }
+
+	        if (type == 'firstleg') {
+
+
+	            if (status == 'done') {
+
+	                Pending.find({ 'transferID': tID, 'type': 'direct' }, function (error, request) {
+	                    console.log("here");
+
+	                    var newPending = new Pending();
+	                    newPending.fileHash = newHash;
+	                    newPending.downloader = request[0].downloader;
+	                    newPending.uploader = mac;
+	                    newPending.fileName = request[0].fileName;
+	                    newPending.fileName = request[0].fileName;
+	                    newPending.transferID = tID;
+	                    newPending.type = 'secondleg';
+
+	                    newPending.save();
+	                    console.log("Pending to delete file added");
+	                    console.log(newPending);
+	                });
+	            }
+
+	            Pending.remove({ "transferID": tID, 'downloader': mac, 'type': 'firstleg' }, function (err, removed) {
+	                console.log(removed);
+	                res.send({ 'status': 'OK', 'text': 'Update Complete' });
+	            });
+
+	        }
+
+	        if (type == 'delete') {
+
+	            Pending.remove({ "transferID": tID, 'downloader': mac, 'type': 'delete' }, function (err, removed) {
+	                console.log(removed);
+	                res.send({ 'status': 'OK', 'text': 'Update Complete' });
+	            });
+
+	        }
+
+
+	    });
+
 	});
 
 }
