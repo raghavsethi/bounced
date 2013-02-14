e456dd3b91ceeed4b205773e1992b8a19520db2c
Raghav Sethi <raghavsethi.rs@gmail.com>
2013-02-14 17:41:49 +0530

    Status fixed

--- users/status.js
+++ users/status.js
@@ -13,55 +13,53 @@ function statusHandler(req,res){
 	var total=5;
 	var result=[];
 	var map=[];
-    var temp=[];
+    var tID=[];
     User.find({ 'ip': req.ip }, { 'mac': 1 }, function (error, users) {
         if (users === undefined || users.length === 0) {
             logger.info('status.js-statusHandler: Cannot find user with IP ' + req.ip, error);
             res.send({ 'status': 'Error', 'text': 'Unregistered user' });
             return;
         }
-        Pending.find({ 'downloader': users[0].mac, 'type': 'direct' }, { 'transferID': 1 }, function (error, pendingRequests) {
-            if (pendingRequests === undefined || pendingRequests.length === 0) {
-                logger.info('status.js-statusHandler: Cannot find user with IP ' + req.ip, error);
+        Pending.find({ 'downloader': users[0].mac, 'type': 'direct' }, { 'transferID': 1 }, function (error, tIDRequests) {
+            if (tIDRequests === undefined || tIDRequests.length === 0) {
+                logger.info('status.js-statusHandler: Cannot find direct pendings with downloader mac ' + users[0].mac, error);
                 console.log('No pending requests found.');
                 res.send(result);
                 return;
             }
-            else {
-                for (var k = 0; k < pendingRequests.length; k++) {
-                    temp = [];
-                    Pending.find({ 'transferID': pendingRequests[k], 'type': { $in: ['firstleg', 'secondleg']} }, function (error, tIDRequests) {
-                        if (tIDRequests === undefined || tIDRequests.length === 0) {
-                            logger.info('status.js-statusHandler: Cannot find user with IP ' + req.ip, error);
-                            console.log('No pending requests found.');
-                            result.push(temp);
-                        }
-                        else {
-                            console.log(tIDRequests);
-                            for (i = 0; i < tIDRequests.length; i++) {
-                                if (map[tIDRequests[i].transferID] === undefined && tIDRequests[i].type === "secondleg") {
-                                    map[tIDRequests[i].transferID] = { 'transferID': tIDRequests[i].transferID, 'hash': tIDRequests[i].fileHash, 'fileName': tIDRequests[i].fileName, 'fileSize': tIDRequests[i].fileSize, 'sent': 1, 'total': 5 };
-                                }
-                                else if (map[tIDRequests[i].transferID] === undefined) {
-                                    map[tIDRequests[i].transferID] = { 'transferID': tIDRequests[i].transferID, 'hash': tIDRequests[i].fileHash, 'fileName': tIDRequests[i].fileName, 'fileSize': tIDRequests[i].fileSize, 'sent': 0, 'total': 5 };
-                                }
-                                else if (tIDRequests[i].type === "secondleg") {
-                                    map[tIDRequests[i].transferID]['sent']++;
-                                }
-                            }
-                            for (var i in map) {
-                                temp.push(map[i]);
-                            }
-                            result.push(temp);
-                        }
-                    });
-                }
-                res.send(result);
-                return
+            for (k in tIDRequests) {
+                tID.push(tIDRequests[k]['transferID']);
             }
+            Pending.find({ 'transferID': { $in: tID}, 'type': { $in: ['firstleg', 'secondleg']} }, function (error, pendingRequests) {
+                if (pendingRequests === undefined || pendingRequests.length === 0) {
+                    logger.info('status.js-statusHandler: Cannot find pendings with TIDs :' + tID, error);
+                    console.log('No pending requests found.');
+                    res.send(result);
+                    return;
+                }
+                else {
+                    console.log(pendingRequests);
+                    for (i = 0; i < pendingRequests.length; i++) {
+                        if (map[pendingRequests[i].transferID] === undefined && pendingRequests[i].type === "secondleg") {
+                            map[pendingRequests[i].transferID] = { 'transferID': pendingRequests[i].transferID, 'hash': pendingRequests[i].fileHash, 'fileName': pendingRequests[i].fileName, 'fileSize': pendingRequests[i].fileSize, 'sent': 1, 'total': 5 };
+                        }
+                        else if (map[pendingRequests[i].transferID] === undefined) {
+                            map[pendingRequests[i].transferID] = { 'transferID': pendingRequests[i].transferID, 'hash': pendingRequests[i].fileHash, 'fileName': pendingRequests[i].fileName, 'fileSize': pendingRequests[i].fileSize, 'sent': 0, 'total': 5 };
+                        }
+                        else if (pendingRequests[i].type === "secondleg") {
+                            map[pendingRequests[i].transferID]['sent']++;
+                        }
+                    }
+                    for (var i in map) {
+                        result.push(map[i]);
+                    }
+                    logger.info('status.js-statusHandler: Sent information regarding ' + result.length + ' transfers for ip ' + req.ip);
+                    res.send(result);
+                    return;
+                }
+ 
+            });
         });
-
-        
     });
 }
 exports.statusHandler =statusHandler;
